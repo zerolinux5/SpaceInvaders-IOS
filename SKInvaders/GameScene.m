94758e9c0eb746c488bb19cac71b1f13f185ea2b
Jesus Magana <maganazero5@gmail.com>
2014-07-01 15:47:18 -0700

    Added the invaders to the screen

--- SKInvaders/GameScene.m
+++ SKInvaders/GameScene.m
@@ -10,6 +10,20 @@
 #import <CoreMotion/CoreMotion.h>
 
 #pragma mark - Custom Type Definitions
+//1
+typedef enum InvaderType {
+    InvaderTypeA,
+    InvaderTypeB,
+    InvaderTypeC
+} InvaderType;
+
+//2
+#define kInvaderSize CGSizeMake(24, 16)
+#define kInvaderGridSpacing CGSizeMake(12, 12)
+#define kInvaderRowCount 6
+#define kInvaderColCount 6
+//3
+#define kInvaderName @"invader"
 
 #pragma mark - Private GameScene Properties
 
@@ -34,9 +48,60 @@
 
 - (void)createContent
 {
+    /*
     SKSpriteNode* invader = [SKSpriteNode spriteNodeWithImageNamed:@"InvaderA_00.png"];
     invader.position = CGPointMake(self.size.width/2, self.size.height/2);
     [self addChild:invader];
+    */
+    [self setupInvaders];
+}
+
+-(SKNode*)makeInvaderOfType:(InvaderType)invaderType {
+    //1
+    SKColor* invaderColor;
+    switch (invaderType) {
+        case InvaderTypeA:
+            invaderColor = [SKColor redColor];
+            break;
+        case InvaderTypeB:
+            invaderColor = [SKColor greenColor];
+            break;
+        case InvaderTypeC:
+        default:
+            invaderColor = [SKColor blueColor];
+            break;
+    }
+    
+    //2
+    SKSpriteNode* invader = [SKSpriteNode spriteNodeWithColor:invaderColor size:kInvaderSize];
+    invader.name = kInvaderName;
+    
+    return invader;
+}
+
+-(void)setupInvaders {
+    //1
+    CGPoint baseOrigin = CGPointMake(kInvaderSize.width / 2, 180);
+    for (NSUInteger row = 0; row < kInvaderRowCount; ++row) {
+        //2
+        InvaderType invaderType;
+        if (row % 3 == 0)      invaderType = InvaderTypeA;
+        else if (row % 3 == 1) invaderType = InvaderTypeB;
+        else                   invaderType = InvaderTypeC;
+        
+        //3
+        CGPoint invaderPosition = CGPointMake(baseOrigin.x, row * (kInvaderGridSpacing.height + kInvaderSize.height) + baseOrigin.y);
+        
+        //4
+        for (NSUInteger col = 0; col < kInvaderColCount; ++col) {
+            //5
+            SKNode* invader = [self makeInvaderOfType:invaderType];
+            invader.position = invaderPosition;
+            [self addChild:invader];
+            //6
+            invaderPosition.x += kInvaderSize.width + kInvaderGridSpacing.width;
+        }
+    }
 }
 
 #pragma mark - Scene Update
